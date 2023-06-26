e20371f796fc04aa9d8fb9658b5ff141389c56d4
viiviids <vidasaboktakin@gmail.com>
2023-06-26 18:41:14 -0400

    navbar.js added

---
+++ navbar.js
@@ -0,0 +1,11 @@
+// navbar.js
+const navbarContainer = document.getElementById('navbar-container');
+
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    navbarContainer.innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
